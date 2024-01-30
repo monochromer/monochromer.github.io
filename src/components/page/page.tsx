ef50ce81389df3cc63b042029169c2e590b4b757
monochromer <monochromer@mail.ru>
2024-01-30 20:37:42 +0500

    Перенос `summary_large_image` выше для распознавания Twitter'ом

--- src/components/page/page.tsx
+++ src/components/page/page.tsx
@@ -17,6 +17,7 @@ export const Page: ComponentType<PageProps> = (props) => {
         <meta charset="utf-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="twitter:card" content="summary_large_image"/>
         <title>{props.title}</title>
         <meta property="og:title" content={props.title} />
         {props.description && (
@@ -25,7 +26,6 @@ export const Page: ComponentType<PageProps> = (props) => {
         {props.image && (
           <meta property="og:image" content={props.image} />
         )}
-        <meta name="twitter:card" content="summary_large_image"/>
         {props.headSlot}
       </head>
       <body className="page__body">
