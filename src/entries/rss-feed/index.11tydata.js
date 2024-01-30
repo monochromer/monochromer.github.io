db38c1ddd1ad716533ab0efc1b5d8561cb40beda
monochromer <monochromer@mail.ru>
2024-01-30 16:59:12 +0500

    Рефакторинг коллекции со статьями

--- src/entries/rss-feed/index.11tydata.js
+++ src/entries/rss-feed/index.11tydata.js
@@ -2,33 +2,26 @@ module.exports = {
   permalink: '/rss-feed.xml',
   articlesToDisplay: 10,
   eleventyComputed: {
-    sortedArticles(data) {
-      const articlesCollection = data?.collections?.articles;
-      if (!articlesCollection || !articlesCollection.length) {
-        return null;
-      }
-    return articlesCollection.toSorted((a, b) => b.data.publishedAt - a.data.publishedAt);
-    },
-
     lastBuildDate(data) {
-      const { sortedArticles } = data;
-      if (!sortedArticles || !sortedArticles.length) {
+      const articles = data?.collections?.articles;
+      if (!articles || !articles.length) {
         return null;
       }
-      return (new Date(sortedArticles[0].publishedAt)).toUTCString();
+      const lastArticle = articles[0];
+      return lastArticle.data.publishedAt.toUTCString();
     },
 
     articles(data) {
-      const { sortedArticles } = data;
-      if (!sortedArticles || !sortedArticles.length) {
+      const articles = data?.collections?.articles;
+      if (!articles || !articles.length) {
         return null;
       }
-      return sortedArticles
+      return articles
         .slice(0, data.articlesToDisplay)
         .map((article) => ({
           title: article.data.title,
           link: data.meta.siteBaseLink + article.page.url,
-          date: new Date(article.data.publishedAt).toUTCString(),
+          date: article.data.publishedAt.toUTCString(),
           content: article.data.contentRawHtml
         }));
     },
